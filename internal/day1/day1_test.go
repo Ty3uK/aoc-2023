694ac82bc365590bdc198a1c9f2de005167350dd
Maksim Karelov <max.karelov@gmail.com>
2024-04-11 20:50:50 +0700

    Day 1

---
+++ internal/day1/day1_test.go
@@ -0,0 +1,26 @@
+package day1
+
+import (
+	"os"
+	"testing"
+)
+
+const answer1 = 54239
+const answer2 = 55343
+
+func TestDay1(t *testing.T) {
+	file, err := os.ReadFile("../../assets/day1.txt")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	sum := Day1(string(file))
+	if sum != answer1 {
+		t.Fatalf("Day 1 sum must be %d, but instead it is %d", answer1, sum)
+	}
+
+	sum = Day1_2(string(file))
+	if sum != answer2 {
+		t.Fatalf("Day 1_2 sum must be %d, but instead it is %d", answer2, sum)
+	}
+}
